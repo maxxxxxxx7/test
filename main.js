9b5389ebfcd8110e0c38c692b37b777a535f7f0b
Max Graham <max.graham7@gmail.com>
2019-02-10 20:08:20 +1100

    Revert "Test"
    This reverts commit b1a7f8e7dd0034f3ac5c977517b1cc062c078cbe.

--- main.js
+++ main.js
@@ -1,41 +1,37 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    addCar: false,
-    cars: [],
-    loading: false,
-    searchCar: "",
-    searchState: ""
-  },
-  methods: {
-    search: function() {
-      app.loading = true;
-      var searchQuery = {
-        car: encodeURIComponent(app.searchCar)
-      };
-      if (app.searchState) {
-        searchQuery.state = app.searchState;
-      };
-      Sheetsu.read("https://sheetsu.com/apis/v1.0su/a4d7192e71fd", {
-        search: searchQuery
-      }).then(function(data) {
-
-          /*
-          Add code here
-          */
-          data = data.filter(item => {
-            return item.car.match(new RegExp(app.searchCar, "gi"))
-          });
-
-          app.cars = data;
-          app.loading = false;
-        },
-        function(err) {
-          console.log(err);
-          app.cars = [];
-          app.loading = false;
-        });
+    el: '#app',
+    data: {
+        addCar: false,
+        cars: [],
+        loading: false,
+        searchCar: "",
+        searchState: ""
     },
-  }
+    methods: {
+        search: function () {
+            app.loading = true;
+            var searchQuery = {
+                car: encodeURIComponent(app.searchCar)
+            };
+            if (app.searchState) {
+                searchQuery.state = app.searchState;
+            };
+            Sheetsu.read("https://sheetsu.com/apis/v1.0su/a4d7192e71fd", {
+                search: searchQuery
+            }).then(function (data) {
+                console.log(data);
+                app.cars = data;
+                app.loading = false;
+            },
+                function (err) {
+                    console.log(err);
+                    app.cars = [];
+                    app.loading = false;
+                });
+        },
+        
+         
+        
+    }
 
 })
